f2a80b82b343cd521fb50208837a2249ab01246e
Dhanesh S P <105306599+dhanesh221@users.noreply.github.com>
2026-09-17 17:20:20 -0400

    Return click count on link creation

--- links/serializers.py
+++ links/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from .models import Link
 
 class LinkSerializer(serializers.ModelSerializer):
-    click_count = serializers.IntegerField(read_only=True)
+    click_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Link
         fields = ["id", "original_url", "short_code", "is_active", "created_at", "click_count"]
         read_only_fields = ["short_code", "created_at", "click_count"]
+
+    def get_click_count(self, link):
+        annotated_count = getattr(link, "click_count", None)
+        return annotated_count if annotated_count is not None else link.clicks.count()
 
     def validate_original_url(self, value):
         if value.lower().startswith(("http://", "https://")):
